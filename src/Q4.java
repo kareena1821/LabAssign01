b16b4b2e7dbf548c4c1850b1fcc557d845b1c2b9
kareena <kareena.gupta27@gmail.com>
2022-09-15 11:46:02 +0530

    Final Commit

--- src/Q4.java
+++ src/Q4.java
@@ -8,10 +8,20 @@ public class Q4 {
         int first=sc.nextInt();
         System.out.println("Enter second number:");
         int second=sc.nextInt();
+        int firstcopy=first;
+        int secondcopy=second;
+        System.out.println("With Temporary Variable:");
+
+        int auxiliary= firstcopy;
+        firstcopy=secondcopy;
+        secondcopy=auxiliary;
+        System.out.println("First Number= "+ firstcopy);
+        System.out.println("Second Number= "+secondcopy);
 
         first=first+second;
         second=first-second;
         first=first-second;
+        System.out.println("With Temporary Variable:");
         System.out.println("First Number= "+ first);
         System.out.println("Second Number= "+second);
         
